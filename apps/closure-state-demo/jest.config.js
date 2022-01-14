493992898a9c43be05cd7244b7a35ac71bbdbb56
911mateo911 <malajmateo@gmail.com>
2022-01-15 00:01:41 +0100

    initial commit

---
+++ apps/closure-state-demo/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'closure-state-demo',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/closure-state-demo',
+};
---
+++ apps/closure-state-demo/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="closure-state-demo" />
+      <div />
+    </>
+  );
+}
+
+export default App;
